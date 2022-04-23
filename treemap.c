6c26befcf25f82cb4d354754e33f0af87193ac4c
benjadiazu <>
2022-04-23 17:39:05 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -184,7 +184,7 @@ Pair * nextTreeMap(TreeMap * tree) {
     else{
         while (tree->current != NULL){
             if (tree->lower_than(aux->parent->pair->key,tree->current->pair->key) == 1){
-                tree->current = tree->current->parent;
+                aux->parent = aux->parent->parent;
             }
             else{
                 return tree->current->pair;
