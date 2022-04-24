012d3b96926acb885d5fafbe03dcb4ee2246c9c9
benjadiazu <>
2022-04-24 05:54:49 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -170,7 +170,6 @@ Pair * upperBound(TreeMap * tree, void* key) {
             UB = UB->left; 
         }
         else if (tree->lower_than(UB->pair->key,key) == 1){
-            auxUB = UB;
             UB = UB->right; 
         }
         else{
