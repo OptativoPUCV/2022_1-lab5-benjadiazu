be2056636043a8c12133b63d1074de2b241758cf
benjadiazu <>
2022-04-24 05:47:26 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -166,19 +166,20 @@ Pair * upperBound(TreeMap * tree, void* key) {
   
     while (UB != NULL){
         
-      
+            printf("UB: %d\n", *(int*)UB->pair->key );
+            printf("aux: %d\n\n", *(int*)auxUB->pair->key );
         if (tree->lower_than(key,UB->pair->key) == 1){
+            printf("CUANDO EL UB ES MAS GRANDE\n");
             auxUB = UB;
             UB = UB->left; 
         }
         else if (tree->lower_than(UB->pair->key,key) == 1){
-            printf("UB: %d\n", *(int*)UB->pair->key );
-            printf("aux: %d\n\n", *(int*)auxUB->pair->key );
+            printf("CUANDO EL UB ES MAS CHICO\n");
             auxUB = UB;
             UB = UB->right; 
         }
         else{
-            printf("SE METE AQUI!\n");
+            printf("SE METE AQUI CUANDO ES IGUAL!\n");
             return UB->pair;
         }
     }
